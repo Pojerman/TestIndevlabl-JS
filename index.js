000f3928cb94949b9aa2fe0a626f568fe716c24b
Danil <45513393+Pojerman@users.noreply.github.com>
2024-04-11 13:56:52 +0300

    update

--- index.js
+++ index.js
@@ -1,30 +1,5 @@
-# TestIndevlabl-JS
+"use strict";
 
-## Task - Technical Assessment JavaScript
-
-## General
-
-Write a function as described below and test scenarios to test its validity.
-
-## Details
-
-Write a function that finds missing number in array. Given array of sequent numbers 0,1,2,3...N with missing member. Function finds a first missing number occurrence in the sequence.
-
-## Example:
-
-| Input                | Output |
-|----------------------|--------|
-| [5, 0, 1, 3, 2]      | 4      |
-| [7, 9, 10, 11, 12]   | 8      |
-| ['f', 5, 0, 1, 3, 2] | NaN    |
-| [5, 0, 1, 3, 2, 1]   | 4      |
-| [0, 1, 3, 2]         | 0      |
-| [-2, 0, 1, 2]        | -1     |
-
-
-## Code
-**_Decision 1_**
-```javascript
 function findMissingNumberDec1(arr) {
     // Проверяем, что все элементы массива являются числами
     if (!arr.every(element => typeof element === 'number')) {
@@ -55,10 +30,8 @@ console.log(findMissingNumberDec1(['f', 5, 0, 1, 3, 2])); // Output: NaN
 console.log(findMissingNumberDec1([5, 0, 1, 3, 2, 1])); // Output: 4
 console.log(findMissingNumberDec1([0, 1, 3, 2])); // Output: 0
 console.log(findMissingNumberDec1([-2, 0, 1, 2])) // Output: -1
-```
-_**Decision 2**_
 
-```javascript
+
 function findMissingNumberDec2(arr) {
     // Проверяем, что все элементы массива являются числами
     if (!arr.every(element => typeof element === 'number')) {
@@ -93,4 +66,3 @@ console.log(findMissingNumberDec2(['f', 5, 0, 1, 3, 2])); // Output: NaN
 console.log(findMissingNumberDec2([5, 0, 1, 3, 2, 1])); // Output: 4
 console.log(findMissingNumberDec2([0, 1, 3, 2])); // Output: 0
 console.log(findMissingNumberDec2([-2, 0, 1, 2])) // Output: -1
-```
